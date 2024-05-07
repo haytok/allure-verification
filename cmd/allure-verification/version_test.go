d22491680e4ce5381083243920b46167e466a4b0
Hayato Kiwata <haytok@amazon.co.jp>
2024-05-07 22:41:18 +0900

    fix

--- cmd/allure-verification/version_test.go
+++ cmd/allure-verification/version_test.go
@@ -9,9 +9,16 @@ import (
 
 func TestSum(t *testing.T) {
 
-	runner.Run(t, "TestSum", func(pt provider.T) {
+	runner.Run(t, "TestSum 0", func(pt provider.T) {
 		actual := sum(3, 5)
 		expected := 8
+
+		pt.Assert().Equal(expected, actual)
+	})
+
+	runner.Run(t, "TestSum 1", func(pt provider.T) {
+		actual := sum(3, 5)
+		expected := 9
 
 		pt.Assert().Equal(expected, actual)
 	})
